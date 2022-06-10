fb792769de4ad298c24d92bacd1854dd00065e9d
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-06-10 23:33:07 +0600

    Update Gaussian blur.cpp

--- Gaussian blur.cpp
+++ Gaussian blur.cpp
@@ -42,7 +42,7 @@ int32_t main(){
     }
     for(int i=1;i<=5;i++){
       for(int j=1;j<=5;j++){
-        cout<<letsTry(i,j)<<' ';
+        cout<<floor(letsTry(i,j))<<' ';
       }
       cout<<'\n';
     }
