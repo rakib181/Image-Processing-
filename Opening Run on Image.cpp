69ea176284e06ce73c40e3ce08512c88db7ad4be
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-06-11 00:08:16 +0600

    Update Opening Run on Image.cpp

--- Opening Run on Image.cpp
+++ Opening Run on Image.cpp
@@ -1,21 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int n=16,m=3;
+const int n=12,m=3;
 
 int fit1(int pixI[m][m],int pixJ[m][m]){
-	int ans=0;
+  int ans=0;
     for(int i=0;i<m;i++){
-    	for(int j=0;j<m;j++){
+      for(int j=0;j<m;j++){
           ans+=(pixI[i][j]*pixJ[i][j]);
-    	}
+      }
     }
     return ans;
 }
 
 bool isFit(int image[n][n],int pix1[m][m],int x,int y){
-	int fitPixelOri=fit1(pix1,pix1);
-	int ans=0;
-	int macroPix[m][m];
+  int fitPixelOri=fit1(pix1,pix1);
+  int ans=0;
+  int macroPix[m][m];
              macroPix[0][0]=image[x-1][y-1];
              macroPix[0][1]=image[x-1][y];
              macroPix[0][2]=image[x-1][y+1];
@@ -27,17 +27,17 @@ bool isFit(int image[n][n],int pix1[m][m],int x,int y){
              macroPix[2][2]=image[x+1][y+1];
 
              for(int i=0;i<m;i++){
-             	for(int j=0;j<m;j++){
-             		ans+=(macroPix[i][j]*pix1[i][j]);
-             	}
+              for(int j=0;j<m;j++){
+                ans+=(macroPix[i][j]*pix1[i][j]);
+              }
              }
              if(ans==fitPixelOri)return true;
              else return false;
 }
 
 bool isFit1(int image[n+3][n+3],int pix1[m][m],int x,int y){
-	int ans=0;
-	int macroPix[m][m];
+  int ans=0;
+  int macroPix[m][m];
              macroPix[0][0]=image[x-1][y-1];
              macroPix[0][1]=image[x-1][y];
              macroPix[0][2]=image[x-1][y+1];
@@ -49,83 +49,83 @@ bool isFit1(int image[n+3][n+3],int pix1[m][m],int x,int y){
              macroPix[2][2]=image[x+1][y+1];
 
              for(int i=0;i<m;i++){
-             	for(int j=0;j<m;j++){
-             		ans+=(macroPix[i][j]*pix1[i][j]);
-             	}
+              for(int j=0;j<m;j++){
+                ans+=(macroPix[i][j]*pix1[i][j]);
+              }
              }
              if(ans!=0)return true;
              else return false;
 }
 
 int32_t main(){
-	 ios_base::sync_with_stdio(false);
- 	 cin.tie(NULL); 
- 	 cout.tie(NULL);
- 	 #ifndef ONLINE_JUDGE 
- 	 freopen("input.txt","r",stdin); 
- 	 freopen("output.txt","w",stdout); 
- 	 #endif
- 	 auto start=clock();
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL); 
+   cout.tie(NULL);
+   #ifndef ONLINE_JUDGE 
+   freopen("input.txt","r",stdin); 
+   freopen("output.txt","w",stdout); 
+   #endif
+   auto start=clock();
      int image[n][n];
      int newImage[n][n];
       int newImage2[n+3][n+3];
         int finalImage[n+1][n+1];
      for(int i=0;i<n;i++){
-     	for(int j=0;j<n;j++){
-     		cin>>image[i][j];
-     		newImage[i][j]=0;
-     		finalImage[i][j]=0;
-     	}
+      for(int j=0;j<n;j++){
+        cin>>image[i][j];
+        newImage[i][j]=0;
+        finalImage[i][j]=0;
+      }
      }
      for(int i=0;i<=n+2;i++){
-     	for(int j=0;j<=n+2;j++){
-     		newImage2[i][j]=0;
-     	}
+      for(int j=0;j<=n+2;j++){
+        newImage2[i][j]=0;
+      }
      }
      int pixel[m][m];
      for(int i=0;i<m;i++){
-     	for(int j=0;j<m;j++){
-     		cin>>pixel[i][j];
-     	}
+      for(int j=0;j<m;j++){
+        cin>>pixel[i][j];
+      }
      }
      for(int i=1;i<n-1;i++){
-     	for(int j=1;j<n-1;j++){
-     		if(isFit(image,pixel,i,j)){
-     			newImage[i][j]=1;
-     		}else{
-     			newImage[i][j]=0;
-     		}
-     	}
+      for(int j=1;j<n-1;j++){
+        if(isFit(image,pixel,i,j)){
+          newImage[i][j]=1;
+        }else{
+          newImage[i][j]=0;
+        }
+      }
      }
      //Letstry Zero Padding
      for(int i=0;i<n+2;i++){
-     	for(int j=0;j<n+2;j++){
+      for(int j=0;j<n+2;j++){
            if( j==0 ||
             i==0 ||
             i==n+1 ||
             j==n+1){
-           	newImage2[i][j]=0;
+            newImage2[i][j]=0;
            }else{
-           	newImage2[i][j]=newImage[i][j];
+            newImage2[i][j]=newImage[i][j];
            }
-     	}
+      }
      }
      for(int i=1;i<n+1;i++){
-     	for(int j=1;j<n+1;j++){
+      for(int j=1;j<n+1;j++){
              if(isFit1(newImage2,pixel,i,j)){
                finalImage[i][j]=1;
              }else{
-             	finalImage[i][j]=0;
+              finalImage[i][j]=0;
              }
-     	}
+      }
      }
      for(int i=0;i<n;i++){
-     	for(int j=0;j<n;j++){
-     		cout<<finalImage[i][j]<<' ';
-     	}
-     	cout<<'\n';
+      for(int j=0;j<n;j++){
+        cout<<finalImage[i][j]<<' ';
+      }
+      cout<<'\n';
      }
 
- 	 cerr<<1.0 * (clock()-start)/CLOCKS_PER_SEC<<endl;
- 	 return 0;
+   cerr<<1.0 * (clock()-start)/CLOCKS_PER_SEC<<endl;
+   return 0;
 }
